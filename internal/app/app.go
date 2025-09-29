59fe40f909a865e0c98374903648f81afeee91dc
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2025-09-30 01:08:02 +0330

    feat(app): implement function type for DI

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,53 +2,92 @@ package app
 
 import (
 	"context"
-	"flowcargo/internal/shared/logger"
 	"net/http"
 	"os"
 	"os/signal"
+
+	"flowcargo/internal/shared/config"
+	"flowcargo/internal/shared/logger"
 )
 
 type App struct {
-	deps   Dependencies
-	srv    Server
-	logger logger.Logger // This is just for convenience
+	cfg  config.Config
+	db   *Database
+	deps Dependencies
+	srv  Server
 }
 
-type wireFunc = func() (Dependencies, error)
+type wireConfig = func() config.Config
 
-func CreateAndRun(envPath string) error {
-	app, err := newApp(wireDependencies)
+// wireDB is a function that wires up the database dependency for the application.
+// It helps for creating alternative implementations of the database dependency. (especially for testing)
+type wireDataBase = func(ctx context.Context, URL string) (*Database, error)
+
+// wireDeps is a function that wires up the dependencies for the application.
+// It helps for creating alternative implementations of the dependencies. (especially for testing)
+type wireDependencies = func(ctx context.Context, db *Database, isDev bool, logLevel logger.LogLevel) (Dependencies, error)
+
+// wireServer is a function that wires up the server dependency for the application.
+// It helps for creating alternative implementations of the server dependency. (especially for testing)
+type wireServer = func(address string, handlers Handlers) Server
+
+// CreateAndRun is lifecycle management for the application.
+// It depends on function type to be replacable
+func CreateAndRun(ctx context.Context, envPath string) error {
+	app, err := newApp(
+		ctx,
+		wireCfg,
+		wireDB,
+		wireDeps,
+		wireSrv,
+	)
 	if err != nil {
 		return err
 	}
-	app.logger.Info("Application created successfully!")
-	app.logger.Infof("Env: %s", envPath)
+	app.Logger().Info("Application created successfully!")
+	app.Logger().Infof("Env: %s", envPath)
 	return app.runApp()
 }
 
-func newApp(fn wireFunc) (App, error) {
-	deps, err := fn()
+// newApp is a factory function that creates a new App instance with all dependencies.
+// By replacing the passing functions, you can change the implementation of the App
+func newApp(
+	ctx context.Context,
+	fConfig wireConfig,
+	fDB wireDataBase,
+	fDeps wireDependencies,
+	fServer wireServer,
+) (App, error) {
+	cfg := fConfig()
+	db, err := fDB(ctx, cfg.GetDatabaseURL())
 	if err != nil {
 		return App{}, err
 	}
-	srv := wireServer()
+	deps, err := fDeps(ctx, db, cfg.IsDevelopment(), cfg.LogLevel())
+	if err != nil {
+		return App{}, err
+	}
+
+	srv := fServer(cfg.ServerAddress(), deps.Handlers)
 	return App{
-		deps:   deps,
-		srv:    srv,
-		logger: deps.Logger,
+		cfg:  cfg,
+		db:   db,
+		deps: deps,
+		srv:  srv,
 	}, nil
 }
 
+// runApp starts all the resources and runs the application.
+// It waits for the application to finish.
 func (a App) runApp() error {
 	// Start server in a goroutine so it doesn't block
 	go func() {
-		a.logger.Infof("Starting server on address %s", a.srv.getAddress())
-		if err := a.srv.start(a.logger); err != nil && err != http.ErrServerClosed {
-			a.logger.Errorf("Server error: %v", err)
+		if err := a.srv.start(); err != nil && err != http.ErrServerClosed {
+			a.Logger().Errorf("Server error: %v", err)
 		}
 	}()
 
-	a.logger.Info("Server started successfully!")
+	a.Logger().Infof("Server started successfully! Listening on address %s", a.srv.getAddress())
 	return a.waitForShutdown(context.Background())
 }
 
@@ -58,16 +97,26 @@ func (a App) waitForShutdown(ctx context.Context) error {
 
 	select {
 	case <-quit:
-		a.logger.Info("Received interrupt signal")
+		a.Logger().Info("Received interrupt signal")
 	case <-ctx.Done():
-		a.logger.Info("Context cancelled")
+		a.Logger().Info("Context cancelled")
 	}
 
 	return a.shutdown(ctx)
 }
 
+// shutdown stops all the resources.
 func (a App) shutdown(ctx context.Context) error {
 	// TODO: Implement dependencies and resources cleanup
-	a.logger.Info("Resources released successfully!")
-	return a.srv.shutdown(ctx)
+	a.db.Close()
+	err := a.srv.shutdown(ctx)
+	if err != nil {
+		return err
+	}
+	a.Logger().Info("Resources released successfully!")
+	return nil
+}
+
+func (a App) Logger() logger.Logger {
+	return a.deps.getLogger()
 }
--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -1,37 +1,57 @@
 package app
 
 import (
-	"testing"
+	"context"
 	"errors"
+	"flowcargo/internal/shared/config"
+	"flowcargo/internal/shared/logger"
+	"testing"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestAppCreateAndRun(t *testing.T) {
 	t.Run("Returns error if it can not wire dependencies", func(t *testing.T) {
-		original := wireDependencies
+		testCtx := context.Background()
+		original := wireDeps
 		defer func() {
-			wireDependencies = original
+			wireDeps = original
 		}()
 		
-		wireDependencies = func() (Dependencies, error) {
+		wireDeps = func(ctx context.Context,db *Database, isDev bool, level logger.LogLevel) (Dependencies, error) {
 			return Dependencies{}, errors.New("test error")
 		}
 		
-		err := CreateAndRun("path")
+		err := CreateAndRun(testCtx, "path")
 		require.Error(t, err)		
 	})
 	
-	t.Run("Creates the application, if wire dependencies don't return error", func(t *testing.T) {
-		original := wireDependencies
+	t.Run("Creates the application, if all wire functions succeed", func(t *testing.T) {
+		testCtx := context.Background()
+		originalDeps := wireDeps
+		originalDB := wireDB
+		originalCfg := wireCfg
+		originalSrv := wireSrv
 		defer func() {
-			wireDependencies = original
+			wireDeps = originalDeps
+			wireDB = originalDB
+			wireCfg = originalCfg
+			wireSrv = originalSrv
 		}()
 		
-		wireDependencies = func() (Dependencies, error) {
+		wireDeps = func(ctx context.Context, db *Database, isDev bool, level logger.LogLevel) (Dependencies, error) {
 			return Dependencies{}, nil
 		}
 		
-		_, err := newApp(wireDependencies)
+		wireDB = func(ctx context.Context, dbURL string) (*Database, error) {
+			return &Database{}, nil
+		}
+		
+		wireCfg = func() (config.Config) {
+			return config.Config{}
+		}
+				
+		_, err := newApp(testCtx, wireCfg, wireDB, wireDeps, wireSrv)
 		require.NoError(t, err)	
 	})
 }
