3fdcfbd7d2c9626e18c95acd30dac489f04a0ba0
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2025-09-30 01:04:22 +0330

    feat(app): pass context to app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"flowcargo/internal/app"
 )
 
 func main() {
 	fmt.Println("Hello from main!")
-	err := app.CreateAndRun("./.env.dev")
+	ctx := context.Background()
+	err := app.CreateAndRun(ctx, "./.env.dev")
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
