f878f98f32d98cc2ae5e7e689548659beac4459a
Mehrnoosh H. Kashani <59601818+mehrnoosh-hk@users.noreply.github.com>
2025-09-30 01:12:05 +0330

    feat(tenant): implement service

--- internal/tenant/service.go
+++ internal/tenant/service.go
@@ -1,2 +1,52 @@
 // Business logic and use cases for tenant management
 package tenant
+
+import (
+	"context"
+	"flowcargo/internal/shared/logger"
+
+	"github.com/google/uuid"
+)
+
+type TenantService interface {
+    CreateTenant(ctx context.Context, params CreateTenantParams) (*Tenant, error)
+    GetTenantByID(ctx context.Context, id uuid.UUID) (*Tenant, error)
+	UpdateTenant(ctx context.Context, id uuid.UUID, params UpdateTenantParams) (*Tenant, error)
+	DeleteTenant(ctx context.Context, id uuid.UUID) (*Tenant, error)
+}
+
+type tenantService struct{
+	l logger.Logger
+	tenantRepo TenantRepository
+}
+
+func NewTenantService(tenantRepo TenantRepository, l logger.Logger) TenantService {
+    return &tenantService{
+		l: l,
+		tenantRepo: tenantRepo,
+	}
+}
+
+func (ts *tenantService) CreateTenant(ctx context.Context, params CreateTenantParams) (*Tenant, error) {
+	return ts.tenantRepo.CreateTenant(ctx, params)
+}
+
+func (ts *tenantService) GetTenantByID(ctx context.Context, id uuid.UUID) (*Tenant, error) {
+	return ts.tenantRepo.GetTenantByID(ctx, id)
+}
+
+func (ts *tenantService) UpdateTenant(ctx context.Context, id uuid.UUID, params UpdateTenantParams) (*Tenant, error) {
+	params.ID = id
+	return ts.tenantRepo.UpdateTenant(ctx, params)
+}
+
+func (ts *tenantService) DeleteTenant(ctx context.Context, id uuid.UUID) (*Tenant, error) {
+	ts.l.Info("Deleting tenant: ", "id = ", id)
+	isActive := false
+	params := UpdateTenantParams{
+		ID:       id,
+		IsActive: &isActive,
+	}
+	ts.l.Info("Deleting tenant params: ", "params = ", params)
+	return ts.tenantRepo.UpdateTenant(ctx, params)
+}
